7ab025ca68171f375185d911560e88297d0b4d67
Archreus <ascendance.development@gmail.com>
2021-01-22 13:44:53 +1300

    Update b3dml320f.js

--- assets/scripts/b3dml320f.js
+++ assets/scripts/b3dml320f.js
@@ -1,14 +1,18 @@
 var subVar;
 
 function showFunction() {
-    subVar = setTimeout(showPage, 2000);
+    subVar = setTimeout(showPage, 000);
 }
 
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("head-title-img").style.display = "block";
+    document.getElementById("head-title-description").style.display = "block";
     //document.getElementById("invite-url").style.display = "block";
     document.getElementById("scroll-btn").style.display = "block";
+    document.getElementById("server-stats").style.display = "block";
+    document.getElementById("user-stats").style.display = "block";
+    document.getElementById("other-stats").style.display = "block";
     document.getElementById("header-nav").style.display = "block";
     document.getElementById("info-section-one").style.display = "block";
     document.getElementById("info-section-two-title").style.display = "block";
