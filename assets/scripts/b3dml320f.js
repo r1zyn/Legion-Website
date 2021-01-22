ace47ea9a25b91c06ab8eb4dac49e6f64080978b
Carson Burke <carsonburke22@gmail.com>
2021-01-21 18:12:02 -0800

    overhualed landing page

--- assets/scripts/b3dml320f.js
+++ assets/scripts/b3dml320f.js
@@ -1,3 +1,41 @@
+window.onscroll = function() {
+    scrolltop()
+};
+
+function scrolltop() {
+
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+
+        document.getElementById("navbar").style.backgroundColor = "var(--nav)";
+        document.getElementById("navbar").style.boxShadow = "rgba(0, 0, 0, 0.05) 0 4px 12px 2px";
+
+        document.getElementById("navbar").style.transition = "all 0.2s";
+
+    } else {
+
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("navbar").style.boxShadow = "none";
+
+        document.getElementById("navbar").style.transition = "all 0.2s";
+
+    }
+}
+
+function navbardivactivate() {
+
+    document.getElementById("navsidebarcontent").classList.toggle("navsidebarcontentactive");
+
+    document.getElementById("navsidebarbutton").classList.toggle("navsidebarbuttonactive");
+
+
+    document.getElementById("bar1").classList.toggle("barclick1");
+
+    document.getElementById("bar2").classList.toggle("barclick2");
+
+    document.getElementById("bar3").classList.toggle("barclick3");
+
+}
+
 var subVar;
 
 function showFunction() {
